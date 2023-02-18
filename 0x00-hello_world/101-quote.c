b8bd286b534bfb4e4721c89485b9609a580bf43d
Pretty-Phardheelart <yusuffadilat2000@gmail.com>
2023-02-18 04:58:13 -0800

    my last resolve

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - This is a programme that prints a line to the standard error
- * Return 1 (Success)
+ * main - This is a programme that prints a line to the
+ * standard error
+ * Return: 1 (Success)
  */
-int main(void
+
+
+int main(void)
+
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+
 }
