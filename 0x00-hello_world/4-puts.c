a193d199d4705f21c221122fb3160f29105895c3
Pretty-Phardheelart <yusuffadilat2000@gmail.com>
2023-02-18 03:31:42 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - This is the program that prints a line with puts function
- * Return: 0 (Sucess)
+ * main - This is the program that prints a line with
+ * puts function
+ * Return: 0 (Success)
  */
+
 int main(void)
+
 {
-puts(""Programming is like building a multilingual puzzle,\n");
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
