a1fd0661ab250e294845f8cc08104906f233c9d2
Pretty-Phardheelart <yusuffadilat2000@gmail.com>
2023-02-21 08:58:58 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
+
 /**
- *__islower(int c) - a function that prints 10 times the alphabet,
- * in lowercase, followed by a new line
+ * _islower - a function that prints 10 times the alphabet, in lowercase
  * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
@@ -12,7 +12,6 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-
 	return (1);
 	}
 	return (0);
